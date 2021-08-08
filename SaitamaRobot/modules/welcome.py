223eb6f113ca21846e58ae4fb469a48e50a53230
naman2007 <79126913+naman2007@users.noreply.github.com>
2021-08-08 15:57:19 +0530

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -566,14 +566,14 @@ def left_member(update: Update, context: CallbackContext):
             # Give the owner a special goodbye
             if left_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "The Almighty Pain Is Leaving Now! But!,He Will Be Back Again!", reply_to_message_id=reply,
+                    "The Almighty God Is Leaving Now! But!,He Will Return for sure to hunt", reply_to_message_id=reply,
                 )
                 return
 
             # Give the devs a special goodbye
             elif left_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "See you later at the Akatsuki Organization!",
+                    "See you later at the Hunters Association!",
                     reply_to_message_id=reply,
                 )
                 return
