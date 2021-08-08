a84acd58c20346ea991a98aed1126a360d4f26d6
naman2007 <79126913+naman2007@users.noreply.github.com>
2021-08-08 21:37:18 +0530

    Update animesitesearch.py

--- SaitamaRobot/modules/animesitesearch.py
+++ SaitamaRobot/modules/animesitesearch.py
@@ -114,7 +114,7 @@ __help__ = """
 • `/kayo`*:* Find anime from animekayo website.
 """
     
-__mod_name__ = "Anime Site Search"
+__mod_name__ = "Anime Site Search🌐"
 KAYO_SEARCH_HANDLER = DisableAbleCommandHandler("kayo", kayo)
 ANIMESPOT_SEARCH_HANDLER = DisableAbleCommandHandler("animespot", animespot)
 ANIMETM_SEARCH_HANDLER = DisableAbleCommandHandler("animetm", animetm)
