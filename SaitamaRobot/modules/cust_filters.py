aedcf89c4b9c569124d1b33ef7def01a32b14a49
naman2007 <79126913+naman2007@users.noreply.github.com>
2021-08-09 13:17:12 +0530

    Update cust_filters.py

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -612,7 +612,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "Filters🔖"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
