60407192712aa24a34466eace7934b41cc8b0ea7
naman2007 <79126913+naman2007@users.noreply.github.com>
2021-08-11 13:17:35 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -499,7 +499,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./SaitamaRobot/resources/LEMONMILK-Bold.ttf"
+        fnt = "./SaitamaRobot/resources/Newretrostyle3d.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
